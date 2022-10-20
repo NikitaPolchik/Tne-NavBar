5fc633af5c206ed8edd232c7ba6fc78b36ef61fc
Your Name <you@example.com>
2022-10-20 22:23:42 +0300

    finished

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
+const navSlide = () => {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".nav-links");
+  const navLinks = document.querySelectorAll(".nav-links li");
+  //toggle nav
+  burger.addEventListener("click", () => {
+    // toggle works like a switch
+    nav.classList.toggle("nav-active");
+    //animate links
+    // an entire thing here is animation delay for each li in nav-links
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinksFade 0.5s ease forwards ${
+          index / 7 + 0.5
+        }s`;
+      }
+    });
+    burger.classList.toggle("toggle");
+  });
+};
 
+navSlide();
